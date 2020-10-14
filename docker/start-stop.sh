a462e69210300cf53c79498420af797ca1bc1ad4
db260179 <db260179@gmail.com>
2020-10-14 23:33:58 +0100

    Add a docker dnsmasq container to push firmware easier for this board Add exploit to access the original firmware

---
+++ docker/start-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+ARGS="--rm --name mi4a-openwrt-recovery -d --cap-add NET_ADMIN --network host -v $PWD/dnsmasq.conf:/etc/dnsmasq.conf -v $PWD/tftpboot:/tftpboot"
+
+case "$1" in
+  start)
+    docker run $ARGS mi4a-dnsmasq:latest
+    ;;
+  stop)
+    docker stop mi4a-openwrt-recovery
+    ;;
+  restart)
+    docker restart mi4a-openwrt-recovery
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}" >&2
+    exit 1
+    ;;
+esac
