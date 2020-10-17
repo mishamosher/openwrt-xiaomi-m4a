1855f702a0c2b13a36b3234c244e6e0a162f9a19
db260179 <db260179@gmail.com>
2020-10-17 13:54:20 +0100

    Add simple build script with commands that will make it happen

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Simple script to build Openwrt image for the Xiaomi Mi Router 4A Gigabit Router"
+
+echo "Update feeds..."
+./scripts/feeds update -a
+
+echo "Install all packages from feeds..."
+./scripts/feeds install -a
+
+echo "Copy default to make image"
+cp Config-Mi4a .config
+
+echo "Set to use default config"
+make defconfig
+
+echo "Download packages before build"
+make download
+
+echo "Start build and log to build.log"
+make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
+
+exit 0
