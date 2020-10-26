42164364387b83d2f20fbeadc069fc0caadec642
db260179 <db260179@gmail.com>
2020-10-26 19:04:35 +0000

    Add build-docker option for upcoming docker build image

--- build.sh
+++ build.sh
@@ -20,6 +20,29 @@ echo "Start build and log to build.log"
 make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
 }
 
+build-docker () {
+echo "Update feeds..."
+./scripts/feeds update -a
+
+echo "Install all packages from feeds..."
+./scripts/feeds install -a
+
+echo "Copy default to make image"
+cp Config-min .config
+
+echo "Set to use default config"
+make defconfig
+
+echo "Download packages before build"
+make download
+
+echo "Start build and log to build.log"
+make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
+
+echo "Copy built images to $HOME/images"
+cp -r bin/targets/ramips/mt7621/ $HOME/images/
+}
+
 build-rebuild () {
 make defconfig
 echo "Start build and log to build.log"
@@ -38,6 +61,9 @@ case "$1" in
   build)
     build-full
     ;;
+  build-docker)
+    build-docker
+    ;;
   build-rebuild)
     build-rebuild
     ;;
@@ -48,7 +74,7 @@ case "$1" in
     clean-full
     ;;
   *)
-    echo "Usage: $0 {build|build-rebuild|clean-min|clean-full}" >&2
+    echo "Usage: $0 {build|build-docker|build-rebuild|clean-min|clean-full}" >&2
     exit 1
     ;;
 esac
