7ba887ed6d20a26da5f5fdb86f7b7bad36ac2ceb
David Bentham <david.bentham@lvs.co.uk>
2020-10-28 10:12:30 +0000

    Merge from master:
    Improve docker image builder to pass arguments on what type of build you want
    Move dockerfiles together and give them names - removing imagebuild folder
    Add config-full from config-custom

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ echo "Update feeds..."
 echo "Install all packages from feeds..."
 ./scripts/feeds install -a
 
-echo "Copy default custom config to make image"
-cp Config-custom .config
+echo "Copy default config full to make image"
+cp Config-full .config
 
 echo "Set to use default config"
 make defconfig
@@ -27,7 +27,7 @@ echo "Update feeds..."
 echo "Install all packages from feeds..."
 ./scripts/feeds install -a
 
-echo "Copy default min config to make image"
+echo "Copy default config min to make image"
 cp Config-min .config
 
 echo "Set to use default config"
@@ -38,29 +38,6 @@ make download
 
 echo "Start build and log to build.log"
 make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
-}
-
-build-docker () {
-echo "Update feeds..."
-./scripts/feeds update -a
-
-echo "Install all packages from feeds..."
-./scripts/feeds install -a
-
-echo "Copy default min to make image"
-cp Config-min .config
-
-echo "Set to use default config"
-make defconfig
-
-echo "Download packages before build"
-make download
-
-echo "Start build and log to build.log"
-make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
-
-echo "Copy built images to $HOME/images"
-cp -r bin/targets/ramips/mt7621/ $HOME/images/
 }
 
 build-rebuild () {
@@ -78,14 +55,11 @@ make distclean
 }
 
 case "$1" in
-  build)
+  build-full)
     build-full
     ;;
   build-min)
     build-min
-    ;;
-  build-docker)
-    build-docker
     ;;
   build-rebuild)
     build-rebuild
@@ -97,7 +71,9 @@ case "$1" in
     clean-full
     ;;
   *)
-    echo "Usage: $0 {build|build-min|build-docker|build-rebuild|clean-min|clean-full}" >&2
+    echo "Usage: $0 {build-full|build-min|build-rebuild|clean-min|clean-full}" >&2
     exit 1
     ;;
 esac
+
+exec "$@"
