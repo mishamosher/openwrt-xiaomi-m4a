d0da8a5cdc37eea8b79d4eb31606e22bac0dda04
David Bentham <david.bentham@lvs.co.uk>
2021-01-15 16:13:25 +0000

    Remove build-dnsmasq script Rename start-stop.sh to tftpserver.sh - better understanding of script Build dnsmasq as option in tftpserver.sh script

--- docker/build-dnsmasq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t mi4a-dnsmasq:latest -f Dockerfile.dnsmasq .
--- docker/tftpserver.sh
+++ docker/tftpserver.sh
@@ -4,6 +4,9 @@
 ARGS="--rm --name mi4a-openwrt-recovery -d --cap-add NET_ADMIN --network host -v $PWD/dnsmasq.conf:/etc/dnsmasq.conf -v $PWD/tftpboot:/tftpboot"
 
 case "$1" in
+  build)
+    docker build -t mi4a-dnsmasq:latest -f Dockerfile.dnsmasq .
+    ;;
   start)
     docker run $ARGS mi4a-dnsmasq:latest
     ;;
@@ -14,7 +17,7 @@ case "$1" in
     docker restart mi4a-openwrt-recovery
     ;;
   *)
-    echo "Usage: $0 {start|stop|restart}" >&2
+    echo "Usage: $0 {build|start|stop|restart}" >&2
     exit 1
     ;;
 esac
