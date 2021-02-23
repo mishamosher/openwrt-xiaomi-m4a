c97e03923bfbdca896e93cc1366e835eb4838418
David Bentham <david.bentham@lvs.co.uk>
2021-02-23 10:42:45 +0000

    build.sh: Add option to build official version based config

--- build.sh
+++ build.sh
@@ -50,6 +50,32 @@ echo "Start build and log to build.log"
 make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
 }
 
+build-official () {
+echo "Update feeds..."
+./scripts/feeds update -a
+
+echo "Install all packages from feeds..."
+./scripts/feeds install -a && ./scripts/feeds install -a
+
+echo "Copy Openwrt official config..."
+release=$(grep -m1 '$(VERSION_NUMBER),' include/version.mk | awk -F, '{ print $3 }' | sed 's/[)]//g')
+
+wget https://downloads.openwrt.org/releases/$release/targets/ramips/mt7621/config.buildinfo -O .config
+
+echo "Set to use default config"
+make defconfig
+
+echo "Download packages before build"
+if [ "$opt" = "nodownload" ]; then
+   echo "Skipping download of packages.."
+else
+   make download
+fi
+
+echo "Start build and log to build.log"
+make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
+}
+
 build-rebuild () {
 make clean
 make defconfig
@@ -72,6 +98,9 @@ case "$1" in
   build-min)
     build-min
     ;;
+  build-official)
+    build-official
+    ;;
   build-rebuild)
     build-rebuild
     ;;
@@ -82,7 +111,7 @@ case "$1" in
     clean-full
     ;;
   *)
-    echo "Usage: $0 {build-full|build-min|build-rebuild|clean-min|clean-full}" >&2
+    echo "Usage: $0 {build-full|build-official|build-min|build-rebuild|clean-min|clean-full}" >&2
     echo "Optional: {nodownload = No downloads of packages}" >&2
     exit 1
     ;;
